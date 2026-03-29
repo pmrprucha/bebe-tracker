38dcebd076962a167534c9153d6640a9ef9c8326
pmrprucha <pmrprucha@gmail.com>
2026-03-30 00:53:46 +0100

    main.jsx

--- bebe-app/src/main.jsx
+++ bebe-app/src/main.jsx
@@ -1,3 +1,4 @@
+// v2
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
@@ -11,4 +12,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </AppProvider>
   </React.StrictMode>
 )
-// v2
